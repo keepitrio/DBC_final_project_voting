0c21d43430a67ef2cf2629be0a8723bad16cca40
Hannie Dong <hdong92@gmail.com>
2017-08-30 17:42:36 -0700

    Create sessions and users controllers, login form, and register form.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,15 @@
 module SessionsHelper
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def logout
+    session[:user_id] = nil
+  end
+
 end
