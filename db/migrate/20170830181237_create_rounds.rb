5962159bc8e2b8c59d36f5fce6b173beafce23df
Rio <rnose512@gmail.com>
2017-08-30 19:03:14 -0700

    Add open boolean column to round table

--- db/migrate/20170830181237_create_rounds.rb
+++ db/migrate/20170830181237_create_rounds.rb
@@ -2,6 +2,7 @@ class CreateRounds < ActiveRecord::Migration[5.1]
   def change
     create_table :rounds do |t|
     	t.integer :round_number
+    	t.boolean :open, default: false
 
       t.timestamps
     end
