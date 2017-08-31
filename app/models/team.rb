d5e87106943add0511daf213a453f35f0a17c5e9
peanutenthusiast <mhdedios@gmail.com>
2017-08-31 12:09:18 -0700

    Fill out controller for index and show, fill out accompanying pages

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,7 @@
 class Team < ApplicationRecord
   belongs_to :pitch, optional: true
-  has_many :students, class_name: "User", foreign_key: :user_id
+  has_many :students, class_name: :User
+  validates :pitch, uniqueness: true
+    # validates :voter, {presence: true, uniqueness: {scope: :pitch}
+    
 end
