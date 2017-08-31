3a80aa41be86554c6e417370b6dec49ff0824868
Rio <rnose512@gmail.com>
2017-08-31 11:40:50 -0700

    Add logic for admin to turn on rounds

--- app/helpers/pitches_helper.rb
+++ app/helpers/pitches_helper.rb
@@ -1,2 +1,11 @@
 module PitchesHelper
+	def open_round_1
+		round = Round.find_by(round_number: 1)
+		round.update(open: true)
+	end
+
+	def open_round_2
+		round = Round.find_by(round_number: 2)
+		round.update(open: true)
+	end
 end
