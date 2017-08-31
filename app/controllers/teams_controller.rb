c85f3e12c1cfeeb6f9aa93a2a43094410e122a3e
Garima Jain <garimaj2108@gmail.com>
2017-08-31 15:06:36 -0700

    Update controller for new, create and edit.

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -5,36 +5,41 @@ class TeamsController < ApplicationController
     if logged_in?
       @teams = Team.all
       render 'index'
-    else 
+    else
       flash[:notice] = "You do not have access to this page."
       redirect_to login_path
-    end 
+    end
   end
 
   def new
-    @team = Team.new
     if logged_in? && admin?
-    else 
+      @team = Team.new
+    else
       flash[:notice] = "You do not have access to this page."
       redirect_to login_path
-    end 
+    end
   end
 
-  def edit
+  def create
     if logged_in? && admin?
-    else 
-      flash[:notice] = "You do not have access to this page."
-      redirect_to login_path
-    end 
+      @team = Team.new(params[:team_details])
+      if @team.save
+        redirect_to teams_path
+      else
+        flash[:notice] = "The team did not get saved"
+        render "new"
+      end
+    end
   end
+
 
   def show
-    if logged_in? 
+    if logged_in?
       @team = Team.find(params[:id])
       render 'show'
-    else 
+    else
       flash[:notice] = "You do not have access to this page."
       redirect_to login_path
-    end 
+    end
   end
 end
