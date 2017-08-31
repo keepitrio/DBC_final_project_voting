453d89807b5e64f13c868acbe6c68dc03e31d455
peanutenthusiast <mhdedios@gmail.com>
2017-08-31 12:15:07 -0700

    delete comment in team model

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,6 +2,5 @@ class Team < ApplicationRecord
   belongs_to :pitch, optional: true
   has_many :students, class_name: :User
   validates :pitch, uniqueness: true
-    # validates :voter, {presence: true, uniqueness: {scope: :pitch}
-    
+
 end
