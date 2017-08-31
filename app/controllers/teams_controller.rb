b668e82c9a1d836938a7b35c3fdf3a7f15a4cb7c
Nestor Ceron <nestor_ceron@yahoo.com>
2017-08-31 12:28:51 -0700

    Add view for teams using test data

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,35 +2,35 @@ class TeamsController < ApplicationController
   include SessionsHelper
 
   def index
-    if logged_in 
-    else 
+    if logged_in
+    else
       flash[:notice] = "You do not have access to this page."
       redirect_to login_path
-    end 
+    end
   end
 
   def new
-    @team = Team.new
-    if logged_in? && admin?
-    else 
-      flash[:notice] = "You do not have access to this page."
-      redirect_to login_path
-    end 
+    # if logged_in? && admin?
+      @team = Team.new
+    # else
+    #   flash[:notice] = "You do not have access to this page."
+    #   redirect_to login_path
+    # end
   end
 
   def edit
     if logged_in? && admin?
-    else 
+    else
       flash[:notice] = "You do not have access to this page."
       redirect_to login_path
-    end 
+    end
   end
 
   def show
-    if logged_in? 
-    else 
+    if logged_in?
+    else
       flash[:notice] = "You do not have access to this page."
       redirect_to login_path
-    end 
+    end
   end
 end
