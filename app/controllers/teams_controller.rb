5a40df85081f8983291b80216b4109046768ab77
Hannie Dong <hdong92@gmail.com>
2017-09-01 08:59:34 -0700

    Add placeholder team form

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -10,13 +10,24 @@ class TeamsController < ApplicationController
   end
 
   def new
-    @team = Team.new
     if logged_in? && admin?
+      @team = Team.new
+      @users = User.all
+      p @pitch_rounds = PitchRound.where(round_id: 2)
     else 
       flash[:notice] = "You do not have access to this page."
       redirect_to login_path
     end 
   end
+
+  def create 
+    @team = Team.new
+    if @team.save 
+      redirect_to teams_path
+    else
+      redirect_to users_path
+    end
+  end 
 
   def edit
     if logged_in? && admin?
