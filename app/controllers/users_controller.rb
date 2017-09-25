c5b57bb31e17606f3da257cad2cd94aa602a2719
Hannie Dong <hdong92@gmail.com>
2017-09-25 09:44:30 -0700

    Add bootstrap to application

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     @user.password = params[:user][:password]
     if @user.save
-      login
+      login_user
       redirect_to pitches_path
     else 
       render 'new'
