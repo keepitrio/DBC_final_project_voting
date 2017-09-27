31e97b321a93a72531adc358be4b57e39400008a
Hannie Dong <hdong92@gmail.com>
2017-09-27 16:07:58 -0700

    Add error handling to sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,20 +2,28 @@ class SessionsController < ApplicationController
   include SessionsHelper
 
   def login
+    if logged_in?
+      redirect_to pitches_path
+    end
   end
 
   def create
-    @user = User.find_by(email: params[:session][:email])
+    @user = User.find_by(email: session_params[:email])
 
-    if @user && @user.authenticate(params[:session][:password])
-      login_user
-      if logged_in? && admin?
-        redirect_to rounds_path
-      else 
-        redirect_to pitches_path
+    if @user 
+      if @user.authenticate(session_params[:password])
+        login_user
+        if logged_in? && admin?
+          redirect_to rounds_path
+        else 
+          redirect_to pitches_path
+        end
+      else
+        @errors = ['email or password is incorrect']
+        render 'login'
       end
-    else
-      @errors = ['email or password is incorrect']
+    else 
+      @errors = ['Please register to access this site.']
       render 'login'
     end
   end
@@ -25,4 +33,8 @@ class SessionsController < ApplicationController
     redirect_to '/'
   end
   
+  private 
+    def session_params
+      params.require(:session).permit(:email, :password)
+    end
 end
